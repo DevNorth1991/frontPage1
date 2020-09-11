2455c21722b4eb687d83312c7eb6aefff6c3c110
gusta <gusta@gmail.com>
2020-09-10 22:55:51 -0300

    primer commit de mi sitio web

---
+++ js/scripting.js
@@ -0,0 +1,11 @@
+$('.menu-btn').click(function() {
+    $('.nav-menu').slideToggle();
+
+});
+
+
+
+
+scrollReveal().reveal('.showCase', { delay: 500 });
+scrollReveal().reveal('.news-cards', { delay: 50 });
+scrollReveal().reveal('.cards-banner', { delay: 500 });
